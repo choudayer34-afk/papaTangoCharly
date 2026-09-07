9ef119faaeeae79d896a7e2ea23335782ecb69bf
choudayer34-afk <choudayer34@gmail.com>
2026-09-07 11:24:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // sur "fetch" plus bas (vague 22 novies) pour le raisonnement complet.
 // Pattern repris d'EnVie (§56/§57 : réutiliser l'existant avant de recréer).
 
-const CACHE_NAME = "pilotage-cache-v30";
+const CACHE_NAME = "pilotage-cache-v31";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -53,7 +53,6 @@ const APP_SHELL = [
   "./js/components/canevas.js",
   "./js/components/weeklyReview.js",
   "./js/components/pomodoroWidget.js",
-  "./js/components/overviewExport.js",
   "./js/components/adminPanel.js",
   "./js/components/copyLink.js",
   "./js/components/inboxBadge.js",
