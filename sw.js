81e4e28dee1ef5b87cab02d2767fb8d7392c4e1a
choudayer34-afk <choudayer34@gmail.com>
2026-09-07 10:21:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // sur "fetch" plus bas (vague 22 novies) pour le raisonnement complet.
 // Pattern repris d'EnVie (§56/§57 : réutiliser l'existant avant de recréer).
 
-const CACHE_NAME = "pilotage-cache-v28";
+const CACHE_NAME = "pilotage-cache-v29";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -57,6 +57,7 @@ const APP_SHELL = [
   "./js/components/adminPanel.js",
   "./js/components/copyLink.js",
   "./js/components/inboxBadge.js",
+  "./js/components/whatsNewBadge.js",
   "./js/components/pilotageSubNav.js",
   "./js/views/dashboard.js",
   "./js/views/inbox.js",
